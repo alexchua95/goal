f6f6cd18dead0245da761b56df7d86ba3146b91a
Byron Duenas <byronduenas@gmail.com>
2015-12-04 14:57:50 -0800

    added editing skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,12 +2,36 @@ class SkillsController < ApplicationController
 
   def new
     @skill_types = SkillType.all
-    @skill = Skill.includes(:skill_type).new
+    @skill = Skill.new
+  end
+
+  def edit
+    @skill_types = SkillType.all
+    @skill = Skill.includes(:skill_type).find(params[:id])
   end
 
   def create
+    @skill_types = SkillType.all
     @skill = current_user.skills.new(skill_params)
     if @skill.save
+      redirect_to user_path(current_user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    if @skill.destroy
+      redirect_to user_path(current_user)
+    else
+
+    end
+  end
+
+  def update
+    @skill = Skill.find(params[:id])
+    if @skill.update(skill_params)
       redirect_to user_path(current_user)
     else
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_path: '/users/auth/facebook/callback', image_size: :normal, info_fields: 'first_name,last_name,email'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_path: '/users/auth/facebook/callback', image_size: { width: 200, height: 200 }, info_fields: 'first_name,last_name,email'
 end
