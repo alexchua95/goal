6861b83575ad1ff9f292f77cc92adf2c3fdd83c7
Byron Duenas <byronduenas@gmail.com>
2016-01-27 10:47:37 -0800

    Finished fixing

--- app/models/landing.rb
+++ app/models/landing.rb
@@ -1,11 +1,19 @@
 class Landing
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :email, type: String
   field :roles, type: Array, default: []
   field :skills, type: Array, default: []
 
+  before_save :remove_empty
+
   def self.roles
     %w(teacher learner)
+  end
+
+  def remove_empty
+    roles.reject!(&:empty?)
+    skills.reject!(&:empty?)
   end
 
   def self.skills
